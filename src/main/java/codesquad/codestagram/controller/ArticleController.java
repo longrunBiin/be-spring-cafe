3776e11e5d3e87b965ed86e64aa8a55b985dd627
jangsubin <eltm521@naver.com>
2025-03-21 11:34:16 +0900

    feat : 로그인하지 않은 사용자는 글 목록만 접근 가능 - 로그인 하지 않았다면 로그인 페이지로 이동

--- src/main/java/codesquad/codestagram/controller/ArticleController.java
+++ src/main/java/codesquad/codestagram/controller/ArticleController.java
@@ -1,8 +1,11 @@
 package codesquad.codestagram.controller;
 
+import static codesquad.codestagram.controller.UserController.SESSIONED_USER;
+
 import codesquad.codestagram.domain.Article;
 import codesquad.codestagram.dto.ArticleDto;
 import codesquad.codestagram.repository.ArticleRepository;
+import jakarta.servlet.http.HttpSession;
 import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,6 +16,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ArticleController {
+    public static final String ARTICLES = "articles";
     private final ArticleRepository articleRepository;
 
     public ArticleController(ArticleRepository articleRepository) {
@@ -29,12 +33,17 @@ public class ArticleController {
     @GetMapping("/")
     public String showArticles(Model model) {
         List<Article> articles = articleRepository.findAll();
-        model.addAttribute("articles", articles);
+        model.addAttribute(ARTICLES, articles);
         return "home";
     }
 
     @GetMapping("articles/{articleId}")
-    public String showArticleDetail(@PathVariable Long articleId, Model model) {
+    public String showArticleDetail(@PathVariable Long articleId, Model model, HttpSession session) {
+        // 로그인 체크
+        if (session.getAttribute(SESSIONED_USER) == null) {
+            return "redirect:/login";
+        }
+        
         Article article = articleRepository.findById(articleId).get();
         model.addAttribute(article);
         return "articles/show";
