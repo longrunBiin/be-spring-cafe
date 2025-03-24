f1c21ed493a35d6cc642c10a7ad0e4f6f404c0c9
jangsubin <eltm521@naver.com>
2025-03-24 10:44:27 +0900

    test : 게시글 작성테스트

--- src/test/java/codesquad/codestagram/service/ArticleServiceTest.java
+++ src/test/java/codesquad/codestagram/service/ArticleServiceTest.java
@@ -4,11 +4,14 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.BDDMockito.given;
 
+import codesquad.codestagram.domain.Article;
 import codesquad.codestagram.domain.User;
 import codesquad.codestagram.dto.ArticleDto;
 import codesquad.codestagram.dto.ArticleDto.ArticleRequestDto;
+import codesquad.codestagram.dto.UserDto.UserRequestDto;
 import codesquad.codestagram.repository.ArticleRepository;
 import codesquad.codestagram.repository.UserRepository;
+import java.time.LocalDateTime;
 import java.util.Optional;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -40,5 +43,26 @@ class ArticleServiceTest {
         assertThat(exception.getMessage()).isEqualTo("유저가 존재하지 않습니다.");
 
     }
+
+    @Test
+    @DisplayName("글을 작성하면 제목, 내용, 작성자가 저장되어야 한다.")
+    void saveArticleTest() {
+        // Given
+        User user = new User("testUser", "password123", "test", "test@example.com");
+        Article article = new Article("test", "testContent", user);
+        given(userRepository.findByUserId("testUser")).willReturn(Optional.of(user));
+        given(articleRepository.findById(1L)).willReturn(Optional.of(article));
+        ArticleDto.ArticleRequestDto requestDto = new ArticleRequestDto("test", "testContent", "testUser");
+
+        // When
+        articleService.saveArticle(requestDto);
+        Article findArticle = articleRepository.findById(1L).get();
+
+        // Then
+        assertThat(findArticle.getTitle()).isEqualTo("test");
+        assertThat(findArticle.getContent()).isEqualTo("testContent");
+        assertThat(findArticle.getUser()).isEqualTo(user);
+    }
+
 
 }
